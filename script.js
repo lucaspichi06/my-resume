8be8274d35a48326400f3f2a0279373935458a15
lpiscitello <lucas.piscitello@mercadolibre.com>
2021-01-20 11:01:25 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+$(function(){
+    window.sr = ScrollReveal();
+  
+    if ($(window).width() < 768) {
+        if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+            $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+        }
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+    } else {
+        sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+    }
+    
+    sr.reveal('.js--fadeInLeft', {
+        origin: 'left',
+        distance: '300px',
+        easing: 'ease-in-out',
+        duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+        origin: 'right',
+        distance: '300px',
+        easing: 'ease-in-out',
+        duration: 800,
+    });
+});
+  
+window.onload = function(){
+  var el =document.querySelector(".floating-nav__wrapper")
+  var position = el.getAttribute("position")
+  if(position == "right"){
+    el.style.right = "1em";
+  }
+  else if(position == "left"){
+    el.style.left = "1em";
+  }
+};
+
+$(document).ready(function() {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+  });
+});
