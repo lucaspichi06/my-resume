999bb9a64fa40519d73d44118e28f1a20ad045ee
lpiscitello <lucas.piscitello@mercadolibre.com>
2021-01-21 18:06:01 -0300

    navbar correction

--- script.js
+++ script.js
@@ -42,19 +42,38 @@ $(function(){
         duration: 800,
     });
 });
-  
-window.onload = function(){
-  var el =document.querySelector(".floating-nav__wrapper")
-  var position = el.getAttribute("position")
-  if(position == "right"){
-    el.style.right = "1em";
-  }
-  else if(position == "left"){
-    el.style.left = "1em";
-  }
-};
 
 $(document).ready(function() {
+  const navbar = document.getElementById("navbar");
+  const navbarToggle = navbar.querySelector(".navbar-toggle");
+
+  function openMobileNavbar() {
+    navbar.classList.add("opened");
+    navbarToggle.setAttribute("aria-label", "Close navigation menu.");
+  }
+
+  function closeMobileNavbar() {
+    navbar.classList.remove("opened");
+    navbarToggle.setAttribute("aria-label", "Open navigation menu.");
+  }
+
+  navbarToggle.addEventListener("click", () => {
+    if (navbar.classList.contains("opened")) {
+      closeMobileNavbar();
+    } else {
+      openMobileNavbar();
+    }
+  });
+
+  const navbarMenu = navbar.querySelector(".navbar-menu");
+  const navbarLinksContainer = navbar.querySelector(".navbar-links");
+
+  navbarLinksContainer.addEventListener("click", (clickEvent) => {
+    clickEvent.stopPropagation();
+  });
+
+  navbarMenu.addEventListener("click", closeMobileNavbar);
+
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
